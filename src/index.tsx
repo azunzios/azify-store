c4745de50c0e38ab72d9d948bb9c4d99121ba4f3
naranggak <angga.narangga@gmail.com>
2025-11-29 10:14:51 +0700

    push sabtu pagi

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
+import { Analytics } from "@vercel/analytics/react"
 import App from './Blog';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <App />
+      <Analytics />
     </StyledEngineProvider>
   </React.StrictMode>
 );
